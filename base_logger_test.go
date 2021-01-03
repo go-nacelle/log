af983c995a1539660a76b468ade01ae51dc42e7d
Eric Fritz <eric@eric-fritz.com>
2021-01-03 16:06:06 -0600

    Fix bad test suite setup (tests were failing to run).

--- base_logger_test.go
+++ base_logger_test.go
@@ -8,9 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type BaseLoggerSuite struct{}
-
-func (s *BaseLoggerSuite) TestLogFormat(t *testing.T) {
+func TestBaseLoggerLogFormat(t *testing.T) {
 	sink := NewMockLogSink()
 	clock := glock.NewMockClock()
 	logger := newTestLogger(sink, LevelDebug, nil, clock, func() {})
@@ -23,14 +21,14 @@ func (s *BaseLoggerSuite) TestLogFormat(t *testing.T) {
 			// Note: this value refers to the line number containing `LogWithFields` in
 			// the test setup above. If code is added before that line, this value must
 			// be updated.
-			"caller":         "log/base_logger_test.go:18",
+			"caller":         "log/base_logger_test.go:15",
 			"sequenceNumber": uint64(1),
 		},
 		"test 1 2 3",
 	))
 }
 
-func (s *BaseLoggerSuite) TestWrappedLoggers(t *testing.T) {
+func TestBaseLoggerWrappedLoggers(t *testing.T) {
 	sink := NewMockLogSink()
 	clock := glock.NewMockClock()
 	logger := newTestLogger(sink, LevelDebug, LogFields{"init": "foo"}, clock, func() {})
@@ -48,7 +46,7 @@ func (s *BaseLoggerSuite) TestWrappedLoggers(t *testing.T) {
 			// Note: this value refers to the line number containing `LogWithFields` in
 			// the test setup above. If code is added before that line, this value must
 			// be updated.
-			"caller":         "log/base_logger_test.go:39",
+			"caller":         "log/base_logger_test.go:36",
 			"sequenceNumber": uint64(1),
 		},
 		"test 1 2 3",
@@ -63,14 +61,14 @@ func (s *BaseLoggerSuite) TestWrappedLoggers(t *testing.T) {
 			// Note: this value refers to the line number containing `LogWithFields` in
 			// the test setup above. If code is added before that line, this value must
 			// be updated.
-			"caller":         "log/base_logger_test.go:40",
+			"caller":         "log/base_logger_test.go:37",
 			"sequenceNumber": uint64(2),
 		},
 		"test 1 2 3",
 	))
 }
 
-func (s *BaseLoggerSuite) TestLogLevelFilter(t *testing.T) {
+func TestBaseLoggerLogLevelFilter(t *testing.T) {
 	sink := NewMockLogSink()
 	clock := glock.NewMockClock()
 	logger := newTestLogger(sink, LevelInfo, nil, clock, func() {})
@@ -78,7 +76,7 @@ func (s *BaseLoggerSuite) TestLogLevelFilter(t *testing.T) {
 	mockassert.NotCalled(t, sink.LogFunc)
 }
 
-func (s *BaseLoggerSuite) TestLogFatal(t *testing.T) {
+func TestBaseLoggerLogFatal(t *testing.T) {
 	sink := NewMockLogSink()
 	clock := glock.NewMockClock()
 	called := false
