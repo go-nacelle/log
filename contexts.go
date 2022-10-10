e5499b59eed997be59aa26d5482ef4f4942a02f0
Kristin Davidson <k@kxd.dev>
2022-10-10 14:42:41 -0500

    Add support for setting and retrieving a logger with contexts. (#8)

---
+++ contexts.go
@@ -0,0 +1,18 @@
+package log
+
+import "context"
+
+type loggerKeyType struct{}
+
+var loggerKey = loggerKeyType{}
+
+func WithLogger(ctx context.Context, logger Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+func FromContext(ctx context.Context) Logger {
+	if v, ok := ctx.Value(loggerKey).(Logger); ok {
+		return v
+	}
+	return NewNilLogger()
+}
